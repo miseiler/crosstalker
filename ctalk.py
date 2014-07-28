b4229af2ee75f855b75f60b3827ff400b13a4860
Michael Seiler <miseiler@gmail.com>
2014-07-28 13:59:23 -0400

    added licensing, prepare for first release

--- ctalk.py
+++ ctalk.py
@@ -1,3 +1,26 @@
+"""
+
+Copyright 2014 Michael Seiler
+Boston University
+miseiler@gmail.com
+
+This file is part of Crosstalker.
+
+Crosstalker is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Crosstalker is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Crosstalker.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
 import sys, auc, auc_perm, scripts, clustio, treeio, getopt, random, os
 
 import numpy   as N
